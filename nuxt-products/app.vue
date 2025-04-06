9d8737f84c69323a321303e4d774046891117609
yura76 <yriy.vorobey@gmail.com>
2025-04-07 02:26:06 +0300

    feat: add products list page with page title

--- nuxt-products/app.vue
+++ nuxt-products/app.vue
@@ -2,5 +2,6 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <NuxtPage/>
   </div>
 </template>
---
+++ nuxt-products/pages/product.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Список продуктів</h1>
+  </div>
+</template>
+
+
+
+<script setup>
+useHead({
+  title: 'Список продуктів'
+})
+</script>
